d5129353de9ab649821a62d4f059568bdcea839a
tmaslen <tom_maslen@hotmail.com>
2018-06-11 09:35:05 +0100

    Got readme working in pip

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ def read(fname):
 setup(
   name = 'featureflagclient',
   packages = ['featureflagclient'],
-  version = '0.3',
+  version = '0.4',
   description = 'Service agnostic featureflag client',
+  long_description=read("README.md"),
+  long_description_content_type='text/markdown',
   author = 'Tom Maslen',
   author_email = 'tom_maslen@hotmail.com',
   url = 'https://github.com/tmaslen/featureflagclient-python',
