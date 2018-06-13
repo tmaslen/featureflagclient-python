c5d14cc01fcb16987287f6c3c922ad74ce94c108
tmaslen <tom_maslen@hotmail.com>
2018-06-13 08:58:43 +0100

    linted project code files

--- featureflagclient/client.py
+++ featureflagclient/client.py
@@ -4,45 +4,59 @@ import json
 
 from typing import Dict, Any
 
+
 class FeatureflagclientError404(Exception):
-	def __init__(self, value):
-		self.value = value
-	def __str__(self):
-		return repr(self.value)
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
 
 class FeatureflagclientErrorMalformedJson(Exception):
-	def __init__(self, value):
-		self.value = value
-	def __str__(self):
-		return repr(self.value)
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
 
 class Featureflagclient:
 
-	features: Dict[str, Any] = {}
+    features: Dict[str, Any] = {}
 
-	source_url: str = ""
+    source_url: str = ""
 
-	def __init__(self, source_url: str = None, local_feature_flags: Dict[str, Any] = None) -> None:
+    def __init__(
+        self,
+        source_url: str = None,
+        local_feature_flags: Dict[str, Any] = None
+    ) -> None:
 
-		self.features = local_feature_flags or {}
+        self.features = local_feature_flags or {}
 
-		if (source_url):
-			self.source_url = source_url
+        if (source_url):
+            self.source_url = source_url
 
-			try: 
-				remote_feature_flag = json.loads(urllib.request.urlopen(source_url).read())
-				for remote_feature in remote_feature_flag:
-					if (remote_feature not in self.features):
-						self.features[remote_feature] = remote_feature_flag[remote_feature]
+            try:
+                remote_feature_flag = json.loads(
+                    urllib.request.urlopen(source_url).read()
+                )
+                for remote_feature in remote_feature_flag:
+                    if (remote_feature not in self.features):
+                        self.features[remote_feature] = remote_feature_flag[remote_feature]
 
-			except urllib.error.HTTPError as e:
-				raise FeatureflagclientError404("404 request not found for " + self.source_url)
-			except json.decoder.JSONDecodeError as e:
-				raise FeatureflagclientErrorMalformedJson("Invalid JSON found at " + self.source_url)
+            except urllib.error.HTTPError as e:
+                raise FeatureflagclientError404(
+                    "404 request not found for " + self.source_url
+                )
+            except json.decoder.JSONDecodeError as e:
+                raise FeatureflagclientErrorMalformedJson(
+                    "Invalid JSON found at " + self.source_url
+                )
 
-
-	def get(self, feature: str) -> Any:
-		if ( feature in self.features):
-			return self.features[feature]
-		else:
-			return None
+    def get(self, feature: str) -> Any:
+        if (feature in self.features):
+            return self.features[feature]
+        else:
+            return None
--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -1,21 +1,21 @@
-import pytest
-
 from featureflagclient.client import Featureflagclient
+
 
 def test_actual_http_request():
 
-	f2c = Featureflagclient("https://featureflag.tech/node/exampleflag.json")
+    f2c = Featureflagclient("https://featureflag.tech/node/exampleflag.json")
 
-	assert f2c.get( "text" ) == "laserwolf"
+    assert f2c.get("text") == "laserwolf"
+
 
 def test_actual_http_request_with_override():
 
-	f2c = Featureflagclient(
-		"https://featureflag.tech/node/exampleflag.json",
-		{
-			"text": "overridden"
-		}
-	)
+    f2c = Featureflagclient(
+        "https://featureflag.tech/node/exampleflag.json",
+        {
+            "text": "overridden"
+        }
+    )
 
-	assert f2c.get( "trueBoolean" ) == True
-	assert f2c.get( "text" ) == "overridden"
+    assert f2c.get("trueBoolean") is True
+    assert f2c.get("text") == "overridden"
